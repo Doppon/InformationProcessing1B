be63c86730fffa978675dd46aa5bef3a7e4611db
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-24 01:08:46 +0900

    ポインタを使った配列の交換処理を追加

---
+++ lesson2/prog2-1b.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+	int *array1=(int *)malloc(sizeof(int)*10000);
+	int *array2=(int *)malloc(sizeof(int)*10000);
+	int i;
+	for(i = 0; i < 10000; ++i){
+		*(array1+i) = 2 * i - 1;
+		*(array2+i) = 2 * i;
+	}
+	int *tmp;
+	int change_times;	
+	for(change_times = 0; change_times < 2000; ++change_times){
+		tmp = array1;
+		array1 = array2;
+		array2 = tmp;
+	}
+	printf("終了");
+	return 0;
+}
