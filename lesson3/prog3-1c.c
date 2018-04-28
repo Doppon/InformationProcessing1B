b80c5afc0089e325b32ec609d6b1c6b894e5f7ed
Doppon <zaxpkuraudo7777@gmail.com>
2018-04-28 14:35:37 +0900

    リーダブルコードに

--- lesson3/prog3-1c.c
+++ lesson3/prog3-1c.c
@@ -5,9 +5,10 @@ int main(void){
     int i;
     i=1;
    
-    if((fp = fopen("prog2-1a-line.txt","w"))==NULL)
+    if((fp = fopen("prog2-1a-line.txt","w"))==NULL){
     	printf("\nファイルをオープンできません。");
-    else{
+    }
+    else {
     	// fprintf(fp, "%s",line);
     	while((fgets(line,sizeof(line),fp))!=NULL){
         fprintf("%d:%s",i,line);
