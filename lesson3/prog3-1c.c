fa01804165e7b16f55bd0b9c1a01fecc592653a1
Doppon <zaxpkuraudo7777@gmail.com>
2018-04-28 14:31:03 +0900

    タイプCを追加

---
+++ lesson3/prog3-1c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void){
+	FILE *fp;
+    char line[1000];
+    int i;
+    i=1;
+   
+    if((fp = fopen("prog2-1a-line.txt","w"))==NULL)
+    	printf("\nファイルをオープンできません。");
+    else{
+    	// fprintf(fp, "%s",line);
+    	while((fgets(line,sizeof(line),fp))!=NULL){
+        fprintf("%d:%s",i,line);
+        i++;
+    }
+
+	fclose(fp);
+    return 0;
+}
