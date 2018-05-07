ba08ac766eb984917cbc44e5bce2e1be617228d3
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-07 23:14:25 +0900

    二元一次方程式を解くプログラムをリファクタリング

--- lesson2/solve.c
+++ lesson2/solve.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int solve(double a, double b, double c, double d, double p, double f);
+double x,y;
+
+int solve(double a, double b, double c, double d, double p, double q);
 
 int main(void){
   double a = 2.0;
@@ -9,17 +11,31 @@ int main(void){
   double p = 15.0;
   double q = 5.0;
 
+  // 出力が(det = 0)になるか調整するテストコード
+  // printf("a:");
+  // scanf("%lf",&a);
+  // printf("b:");
+  // scanf("%lf",&b);
+  // printf("c:");
+  // scanf("%lf",&c);
+  // printf("d:");
+  // scanf("%lf",&d);
+
   solve(a, b, c, d, p, q);
+  printf("x = %f\n",x);
+  printf("y = %f\n",y);
 }
 
-int solve(double a, double b, double c, double d, double p, double f){
+int solve(double a, double b, double c, double d, double p, double q){
   double det;
   det = a * d - b * c;
   if(det == 0){
     printf("det = 0");
     return 0;
   }
-  else{     
+  else{
+    x = (1 / det) * ( p * d + q * (-b) );
+    y = (1 / det) * ( p * (-c) + q * a );
     return 1;
   }
 }
