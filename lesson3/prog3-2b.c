1d29687de44f309beeffc8c03214ccfecceb8ae9
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-12 23:24:18 +0900

    改良プログラムを追加

---
+++ lesson3/prog3-2b.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(void){
+  struct _pref{
+    char name[50];
+    int population;
+  };
+  typedef struct _pref PREF;
+  FILE *csv;
+  int sum = 0;
+  int sum2 = 0;
+  int i = 0, j;
+  double ave,bunsan;
+  csv=fopen("2018_population.csv", "r");
+  if(csv==NULL){
+    printf("ファイルが開けませんでした。");
+    return -1;
+  }
+  PREF list[50];
+  while(fscanf(csv, "%s %d", &list[i].name, &list[i].population)!=EOF){
+    i++;
+  }
+  PREF a;
+  for (i = 0 ; i < 47; i++){
+    for(j=i+1; j < 47; j++){
+      if(list[i].population > list[j].population){
+        a = list[i];
+        list[i]=list[j];
+        list[j]=a;
+      }
+    }
+  }
+
+  for (i = 0; i < 20; i++){
+    printf("%d件目：%s %d\n", i+1, list[i].name, list[i].population);
+  }
+  fclose(csv);
+  return 0;
+}
