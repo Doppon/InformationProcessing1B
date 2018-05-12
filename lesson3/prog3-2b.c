6a48e114c366cb166ad19a9c74402276672c40a7
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-12 23:37:05 +0900

    変数名を改良

--- lesson3/prog3-2b.c
+++ lesson3/prog3-2b.c
@@ -7,8 +7,8 @@ int main(void){
     int population;
   };
   typedef struct _pref PREF;
-  PREF list[50];
-  PREF a;
+  PREF scan_list[50];
+  PREF sort_list;
 
   csv = fopen("2018_population.csv", "r");
   if(csv == NULL){
@@ -16,22 +16,22 @@ int main(void){
     return -1;
   }
 
-  while(fscanf(csv, "%s %d", list[i].name, &list[i].population) != EOF){
+  while(fscanf(csv, "%s %d", scan_list[i].name, &scan_list[i].population) != EOF){
     i++;
   }
 
   for (i = 0 ; i < 47; i++){
     for(j = i+1; j < 47; j++){
-      if(list[i].population > list[j].population){
-        a = list[i];
-        list[i]=list[j];
-        list[j]=a;
+      if(scan_list[i].population > scan_list[j].population){
+        sort_list = scan_list[i];
+        scan_list[i] = scan_list[j];
+        scan_list[j] = sort_list;
       }
     }
   }
 
   for (i = 0; i < 20; i++){
-    printf("%d件目：%s %d\n", i+1, list[i].name, list[i].population);
+    printf("%d件目：%s %d\n", i+1, scan_list[i].name, scan_list[i].population);
   }
 
   fclose(csv);
