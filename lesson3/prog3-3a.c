d8e98866e4c1a7a7b5eefd61a1f810f8a0073b65
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-13 03:09:01 +0900

    ファイルのコピー処理を実装

--- lesson3/prog3-3a.c
+++ lesson3/prog3-3a.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 int main(int argc, char *argv[]){
-  FILE *fp;
+  FILE *origin_file;
+  FILE *copy_file;
   char line[1000];
   int i = 1;
 
-  fp = fopen(argv[1], "r");
-  if(fp == NULL){
+  origin_file = fopen(argv[1], "r");
+  if(origin_file == NULL){
     printf("ファイルが開けませんでした。\n");
     return -1;
   }
 
+  copy_file = fopen(argv[2], "w");
+  
+  while((fgets(line, sizeof(line), origin_file)) != NULL){
+    fprintf(copy_file, "%d:%s", i,line);
+    i++;
+  }
+
   printf("コピー元ファイル：%s, コピー先ファイル：%s", argv[1], argv[2]);
 
-  fclose(fp);
+  fclose(origin_file);
+  fclose(copy_file);
   return 0;
 }
