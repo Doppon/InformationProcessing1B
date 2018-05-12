62c71ae0ee04454cd9d3de424d2c62de96460c89
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-12 23:46:58 +0900

    適切な変数名に変更

--- lesson3/prog3-2a.c
+++ lesson3/prog3-2a.c
@@ -5,20 +5,20 @@ int main(void){
     int population;
   };
   typedef struct _pref PREF;
-  FILE *fp;
+  FILE *csv;
   int sum = 0;
   int sum2 = 0;
   int i = 0;
   double ave,bunsan;
 
-  fp=fopen("2018_population.csv", "r");
-  if(fp==NULL){
+  csv=fopen("2018_population.csv", "r");
+  if(csv==NULL){
     printf("ファイルが開けませんでした。");
     return -1;
 	}
 
   PREF list[50];
-  while(fscanf(fp, "%s%d", list[i].name, &list[i].population)!=EOF){
+  while(fscanf(csv, "%s%d", list[i].name, &list[i].population)!=EOF){
     sum=sum+list[i].population;
     i++;
   }
@@ -28,9 +28,9 @@ int main(void){
   for (i = 0 ; i < 47; i ++){
     sum2 = sum2+(list[i].population-ave)*(list[i].population-ave);
   }
-	
+
   bunsan = sum2/47.0;
   printf("平均:%f, 分散:%f\n", ave, bunsan);
-  fclose(fp);
+  fclose(csv);
 	return 0;
 }
