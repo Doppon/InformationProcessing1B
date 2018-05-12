01f5f053c9c05310495ac49edc52b3d949820a15
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-13 00:14:01 +0900

    リファクタリング

--- lesson3/prog3-2a.c
+++ lesson3/prog3-2a.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-int main(void){
+	int main(void){
+  FILE *csv;
+
   struct _pref{
     char name[50];
     int population;
   };
   typedef struct _pref PREF;
-  FILE *csv;
   int sum = 0;
   int sum2 = 0;
-  int i = 0;
+  int i;
   double ave,bunsan;
 
-  csv=fopen("2018_population.csv", "r");
-  if(csv==NULL){
+  csv = fopen("2018_population.csv", "r");
+  if(csv == NULL){
     printf("ファイルが開けませんでした。");
     return -1;
 	}
 
   PREF list[50];
-  while(fscanf(csv, "%s %d", list[i].name, &list[i].population)!=EOF){
+  while(fscanf(csv, "%s %d", list[i].name, &list[i].population) !=EOF){
     sum += list[i].population;
     i++;
   }
