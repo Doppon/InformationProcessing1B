b19ea201faf16bb36c5aa730ca60424b49635cf0
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-13 02:51:37 +0900

    コピー元のファイルを開く処理を追加

--- lesson3/prog3-3a.c
+++ lesson3/prog3-3a.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main(int argc, char *argv[]){
-  
+  FILE *fp;
+  char line[1000];
+
+  fp = fopen(argv[1], "r");
+  if(fp == NULL){
+    printf("ファイルが開けませんでした。\n");
+    return -1;
+  }
+
   printf("コピー元ファイル：%s, コピー先ファイル：%s", argv[1], argv[2]);
 
 }
