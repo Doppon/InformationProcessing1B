c9124f9f9d530c97b8190687d41ab46ed67c047a
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-12 23:45:16 +0900

    リファクタリング

--- lesson3/prog3-2a.c
+++ lesson3/prog3-2a.c
@@ -10,20 +10,25 @@ int main(void){
   int sum2 = 0;
   int i = 0;
   double ave,bunsan;
+
   fp=fopen("2018_population.csv", "r");
   if(fp==NULL){
     printf("ファイルが開けませんでした。");
     return -1;
 	}
+
   PREF list[50];
   while(fscanf(fp, "%s%d", list[i].name, &list[i].population)!=EOF){
     sum=sum+list[i].population;
     i++;
   }
+
   ave = sum/47.0;
+
   for (i = 0 ; i < 47; i ++){
     sum2 = sum2+(list[i].population-ave)*(list[i].population-ave);
   }
+	
   bunsan = sum2/47.0;
   printf("平均:%f, 分散:%f\n", ave, bunsan);
   fclose(fp);
