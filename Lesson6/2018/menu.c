2d4e8b891e7bcad4c7d8633a56ef46a88abbf59e
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-05 01:39:30 +0900

    リファクタリング

--- Lesson6/2018/menu.c
+++ Lesson6/2018/menu.c
@@ -4,33 +4,36 @@
 #include <time.h>
 
 void menu(){
-	int n,success;
+	int n, success;
 	char ch;
 	DATA *dataArray = (DATA *)malloc(sizeof(DATA) * MAXLEN);
-	int ret=1,score;
+	int ret=1, score;
 	srand((unsigned)time(NULL));
 
 	printf("クイズ:q タイピング:t 終了:e\n");
 	printf("menu> ");
 
-	while(ret!=0){
+	while(ret == 1){
 		ch = getchar();
 
 		switch(ch){
 			case '\n':
-				printf("menu> ");
+				// 改行時に照準を合わせるため追記
+				printf("menu> ");				
         break;
 
 			case 'q':
 				input();
+				printf("menu> \n");				
 				break;
 
 			case 't':
-				success=DataReader(dataArray, &n);
+				success = DataReader(dataArray, &n);
 				if(success==0){
 					score=typing(dataArray,n);
 					printf("You got score:%d\n", score);
 				}
+				printf("menu> \n");
 				break;
 
 			case 'e':
